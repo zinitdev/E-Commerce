deeae9e75b6bdb9718a7adbed5969d31d69d3bc3
Lê Hiển Vinh <zin.it.dev@gmail.com>
2023-07-08 17:11:46 +0700

    Flaskr #2: Pass data from controller to template 🌶

--- app/app.py
+++ app/app.py
@@ -1,6 +1,21 @@
 from app import app
 from flask import render_template
+from app import utils
+
+@app.context_processor
+def utility_processor():
+    return dict(
+            format_price = utils.format_price,
+        )
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    categories = utils.load_categories()
+    
+    return render_template('index.html', categories=categories)
+
+@app.route('/products')
+def products():
+    products = utils.load_products()
+    
+    return render_template('products.html', products=products)
---
+++ app/utils.py
@@ -0,0 +1,15 @@
+import json, os
+from app import app
+
+def read_json(path):
+    with open(path, "r") as f:
+        return json.load(f)
+
+def load_categories():
+    return read_json(os.path.join(app.root_path, 'static/data/categories.json'))
+
+def load_products():
+    return read_json(os.path.join(app.root_path, 'static/data/products.json'))
+
+def format_price(amount, currency="$"):
+    return "{1}{0:,.1f}".format(amount, currency)
